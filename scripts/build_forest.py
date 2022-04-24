12f69fc90a83fcb92cd79123e3fde492d7b2305a
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-04-24 17:06:25 +0300

    add scripts

--- scripts/build_forest.py
+++ scripts/build_forest.py
@@ -1,8 +1,5 @@
 import torch
 import torchvision.transforms as T
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import pickle
 
@@ -16,7 +13,12 @@ if __name__ == "__main__":
     df = pd.read_csv('data/full_clear_data.csv')
     all_imgs = df.local_path.to_list()
     ids = df.id.to_list()
-    transforms = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
+    transforms = T.Compose([T.Resize(256),
+                            T.CenterCrop(224),
+                            T.RandomHorizontalFlip(),
+                            T.ToTensor(),
+                            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                            ])
 
     full_dataset = FolderDataset(all_imgs, ids, transforms)
     batch_size = 100
@@ -26,7 +28,7 @@ if __name__ == "__main__":
 
     feature_model = FeatureExtractor(model, layers=['classifier.5'])
 
-    table = HashBuilderFromModel(feature_model, 'cuda', full_dataset, 128, 10, 0.5)
+    table = HashBuilderFromModel(feature_model, 'cuda', full_dataset, 50, 10, 0.75)
     tree, min_hash = table.build_forest(full_loader)
     with open('checkpoints/tree_db.pt', 'wb') as f:
         f.write(pickle.dumps(tree))
