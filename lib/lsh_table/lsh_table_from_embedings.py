1df7bf9983416cb4a85ac2b1da834ae134a106b3
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-05-02 21:21:10 +0300

    optimize hash calculation

--- lib/lsh_table/lsh_table_from_embedings.py
+++ lib/lsh_table/lsh_table_from_embedings.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Tuple
-from datasketch import MinHash, MinHashLSH, MinHashLSHForest
+from datasketch import MinHash, MinHashLSH, MinHashLSHForest, WeightedMinHashGenerator
+from multiprocessing import Pool
 from tqdm import tqdm
 import torch
 import torch.nn as nn
@@ -22,6 +23,7 @@ class HashBuilderFromModel:
         self.num_permutations = num_permutations
         self.tree = MinHashLSHForest(num_perm=num_permutations, l=num_prefix_trees)
         self.hash_lsh = MinHashLSH(threshold=lsh_threshold, num_perm=num_permutations)
+        self.hash_gen = WeightedMinHashGenerator(dim=2048, sample_size=num_permutations, seed=42)
 
     def build_forest(self,
                      full_loader: DataLoader) -> Tuple[MinHashLSHForest, MinHashLSH]:
@@ -32,17 +34,32 @@ class HashBuilderFromModel:
             for (train_img, target_img) in tqdm(full_loader):
                 train_img = train_img.to(self.device)
                 model_output = self.model(train_img).cpu()
-                for image in model_output:
-                    image = image.squeeze(0).detach().ceil().int().numpy()
-                    image_hash = self.get_min_embeding(image, self.num_permutations)
-                    self.tree.add(self.dataset.get_id(iter_num), image_hash)
-                    self.hash_lsh.insert(self.dataset.get_id(iter_num), image_hash)
-                    iter_num += 1
-        return self.tree, self.hash_lsh
+                self.batch_query(iter_num, model_output)
+                iter_num += len(model_output)
+        return self.tree, self.hash_lsh, self.hash_gen
 
-    @staticmethod
-    def get_min_embeding(embeging: np.ndarray, permutations: int) -> np.ndarray:
-        value = MinHash(num_perm=permutations)
+    def query_img_weighted(self,
+                           iter_num: int,
+                           feature: np.ndarray) -> None:
+        image_hash = self.hash_gen.minhash(feature)
+        self.tree.add(self.dataset.get_id(iter_num), image_hash)
+        self.hash_lsh.insert(self.dataset.get_id(iter_num), image_hash)
 
-        value.update(embeging)
-        return value
+    def query_img_simple(self,
+                         iter_num: int,
+                         feature: np.ndarray) -> None:
+        image_hash = MinHash(num_perm=self.num_permutations)
+        image_hash.update(feature)
+        self.tree.add(self.dataset.get_id(iter_num), image_hash)
+        self.hash_lsh.insert(self.dataset.get_id(iter_num), image_hash)
+
+    def batch_query(self, iter_num: int, features: torch.Tensor) -> None:
+
+        def to_numpy(batch_elem: torch.Tensor) -> np.ndarray:
+            return batch_elem.squeeze().detach().ceil().int().numpy()
+
+        batch_features = np.array(list(map(to_numpy, features)))
+
+        with Pool() as pool:
+            pool_data = [(iter_num + idx, feature) for idx, feature in enumerate(batch_features)]
+            pool.starmap(self.query_img_simple, pool_data)
