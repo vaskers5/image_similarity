2a5f8a862792ada9734b8d230c17b2c4d09fba12
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2022-05-09 18:46:16 +0300

    add full configuration from conf

--- test_loading.py
+++ test_loading.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     with open('configs/img_save_conf.json') as f:
         conf = json.load(f)
 
-    folder_path = '/mnt/0806a469-d019-4d6a-be45-7cff5d66eb22/datasets/60m_tokens_set_1'
-    dataset_path = f'{folder_path}/30m_data.parquet.gzip'
-    df = pd.read_parquet(f'{folder_path}/30m_data.parquet.gzip')
+    # folder_path = '/mnt/0806a469-d019-4d6a-be45-7cff5d66eb22/datasets/60m_tokens_set_1'
+    # dataset_path = f'{folder_path}/30m_data.parquet.gzip'
+    # df = pd.read_parquet(f'{folder_path}/30m_data.parquet.gzip')
 
-    # folder_path = 'data/test_loading_folder'
-    # dataset_path = 'data/test_infer.csv'
-    # df = pd.read_csv(dataset_path)
-    # del df['local_path']
+    folder_path = 'data/test_loading_folder'
+    dataset_path = 'data/test_infer.csv'
+    df = pd.read_csv(dataset_path)
+    del df['local_path']
 
-    loader = CloudImgDatasetLoader(df, num_splits=2, batch_size=5000, img_save_conf=conf)
+    loader = CloudImgDatasetLoader(df, **conf)
     loader(folder_path)
